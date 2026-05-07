5a37afd426fce59078f03cab5d30f9e99df81a38
Djomnang Emmanuella joyce <joycedjomnang@gmail.com>
2026-05-07 12:51:59 +0100

    8

--- backend/src/Controller/HomeController.php
+++ backend/src/Controller/HomeController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): RedirectResponse
+    public function index(ParameterBagInterface $params): RedirectResponse
     {
-        // Redirige vers l'application Angular (frontend citoyen)
-        return new RedirectResponse('http://localhost:4200');
+        $frontendUrl = $params->get('frontend_url');
+        return new RedirectResponse($frontendUrl);
     }
 }
