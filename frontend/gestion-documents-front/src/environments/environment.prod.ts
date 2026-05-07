6b2e454e8ac089d9788a115c48f2cf632d842504
Djomnang Emmanuella joyce <joycedjomnang@gmail.com>
2026-05-07 09:20:53 +0100

    6

--- frontend/gestion-documents-front/src/environments/environment.prod.ts
+++ frontend/gestion-documents-front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 //C:\TP_PROJET_B1_a_B3\projet_Angular\gestion_documents_Symphony\frontend\gestion-documents-front\src\environments\environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-api-production-5166.up.railway.app/api', 
+  apiUrl: 'https://backend-api-production-5166.up.railway.app/api',
 };
