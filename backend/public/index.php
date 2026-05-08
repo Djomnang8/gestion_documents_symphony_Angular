fa3fd679ecadd0fedb9784b11cebf0cf246d885a
Djomnang Emmanuella joyce <joycedjomnang@gmail.com>
2026-05-08 14:59:45 +0100

    4

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,14 +1,15 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
-// Désactiver le Runtime pour éviter les erreurs
-$_SERVER['APP_RUNTIME'] = 'Symfony\\Component\\HttpKernel\\Kernel';
 $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? '1';
 $_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'dev';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
